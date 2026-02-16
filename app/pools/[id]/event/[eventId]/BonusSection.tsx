04643e4dd156c0218bc10de359e3870a5121f5a1
Doeks050 <nicktalens@outlook.com>
2026-02-16 09:37:24 +0100

    Update BonusSection.tsx

--- app/pools/[id]/event/[eventId]/BonusSection.tsx
+++ app/pools/[id]/event/[eventId]/BonusSection.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
-import { supabase } from "@/lib/supabaseClient";
+import { supabase } from "../../../../../lib/supabaseClient";
 
 export default function BonusSection() {
   const params = useParams<{ id: string; eventId: string }>();
